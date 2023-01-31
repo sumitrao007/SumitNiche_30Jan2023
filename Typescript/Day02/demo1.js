5bf4920fc11933cfc9d08118fc1b8c1a74cea8f4
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-01-31 22:00:21 +0530

    Day02

---
+++ Typescript/Day02/demo1.js
@@ -0,0 +1,38 @@
+// console.log("Hello World");
+// console.log('Welcome to Angular 12 Module');
+//variable
+var a1, a2;
+var a_1, a$2;
+//data type
+//number(int,long,double,short,float),string(''/""),boolean(true/false)
+//any => number,string,boolean
+//void => neutral data type=> it does not point any thing 
+//mise. null & undefined => it used as value as well as data type 
+var a = 10; //forward declration & defination
+// console.log("Value of a is "+a);
+// console.log(`Value of a= ${a} `);
+var str;
+// console.log('Value of str '+str);
+str = 'Sumit';
+// console.log('Value of str '+str);
+var myany;
+myany = 2.5;
+// console.log("Value of myany "+myany);
+myany = 'Sumit';
+// console.log("Value of myany "+myany);
+myany = true;
+// console.log("Value of myany "+myany);
+var c;
+var c1 = 85;
+// c1='';
+var c3;
+c3 = 85;
+//literal type
+var b;
+b = 50;
+// console.log("value of b is "+b);
+b = 'Sumit';
+// console.log("value of b is "+b);
+var b1;
+b1 = null;
+console.log("b1 =" + b1);
---
+++ Typescript/Day02/demo1.ts
@@ -0,0 +1,53 @@
+
+// console.log("Hello World");
+// console.log('Welcome to Angular 12 Module');
+
+//variable
+var a1,a2;
+var a_1,a$2;
+
+//data type
+//number(int,long,double,short,float),string(''/""),boolean(true/false)
+//any => number,string,boolean
+//void => neutral data type=> it does not point any thing 
+//mise. null & undefined => it used as value as well as data type 
+
+var a:number=10;  //forward declration & defination
+// console.log("Value of a is "+a);
+// console.log(`Value of a= ${a} `);
+
+var str!:string;
+// console.log('Value of str '+str);
+str='Sumit'
+// console.log('Value of str '+str);
+
+var myany:any;
+myany=2.5;
+// console.log("Value of myany "+myany);
+myany='Sumit';
+// console.log("Value of myany "+myany);
+myany=true;
+// console.log("Value of myany "+myany);
+
+var c;
+
+var c1=85;
+// c1='';
+
+var c3;
+c3=85;
+
+//literal type
+var b:number|string;
+b=50;
+// console.log("value of b is "+b);
+b='Sumit';
+// console.log("value of b is "+b);
+
+var b1:string|null;
+b1=null;
+console.log("b1 ="+b1)
+b1='sdg';
+
+var b3:50|string;
+b3=50;
