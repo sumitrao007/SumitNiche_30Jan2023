c4bd83ee3d0909f612d8e64b81f40c7edef14691
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-02-01 21:49:24 +0530

    Day03

---
+++ Typescript/Day03/demo1.ts
@@ -0,0 +1,83 @@
+//Type assertion
+// pre-requiest=> any/unknown/object 
+// 
+
+var a:any;
+
+// a.
+// 1 Angle bracket
+    // typescript file 
+
+    var temp=(<string> a);
+    // temp.
+
+// 2 as syntax
+// ts file as well as html file 
+
+var temp1=(a as string);
+// temp1.
+
+//Operators
+// Airthamthic=> +,-,*,/,%
+//logical => &&,||
+//bitwise => !,~,^,&,|,<<,>>
+//relational => <,>,
+//conditional => <=,>=,!=,==(it checks only value),===(strongly equality => it checks value as well as data type )
+//ternary=> condition?expression1:expression2
+//unary => post/pre inc/dec ++a,a--
+//Assignment => =,+=,-=,/=,*=,%=
+
+//control statement
+// if-else,if-else if ,switch,break,continue
+
+    var rating=21;
+    // if (rating<4){
+    //     console.log("Condition is true");
+    // }else{
+    //     console.log("Condition is false");
+    // }
+
+    var choice=21;
+    // switch(choice){
+    //     case 1:console.log("U r in case 1")
+    //         break;
+    //     case 2: console.log("U r in case 2")
+    //         break;
+    //     default:console.log("U r in default case ");
+    //         break;
+
+    // }
+
+    //loop statement
+    // while,do-while,for,foreach
+
+    var count=5;
+        // while(count!=0){
+        //     console.log("Count is "+count);
+        //     count--;
+        // }
+
+// do{
+//     console.log("====Count is "+count);
+//             count--;
+// }while(count!=0)
+
+// for
+    // for(var i=0;i<5;i++){
+    //     console.log("value of i is "+i)
+    // }
+    // console.log("i value after loop "+i);
+    //var => it is global scope
+
+    // let => to create variable 
+    // scope => within nearest block
+
+    for(let i=0;i<5;i++){
+        console.log("value of i is "+i)
+        
+    }
+    // console.log("i value after loop "+i);
+
+    //const => u can't change the value of variable once u assign 
+    //scope => global as well as local 
+        const pi=3.14;
---
+++ Typescript/Day03/demo2.ts
@@ -0,0 +1,16 @@
+
+//Function
+// 1 Function without parameter & without return type
+// 2 Function with parameter & without return type
+// 3 Function without parameter & with return type
+// 4 Function with parameter & with return type
+
+// 1 Function without parameter & without return type
+
+    function add1(){
+        console.log("U r first type function")
+        
+    }
+
+
+    add1();
