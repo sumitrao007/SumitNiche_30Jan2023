2e8bfcfd74500359279742c870cc01e62dde7023
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-02-09 21:51:34 +0530

    Day07

---
+++ Typescript/Day07/demo2.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var demo1_1 = require("./demo1");
+var obj = new demo1_1.Myclass(3, 'Spruha', 'Raokhande');
+obj.display();
+console.log("Addition " + (0, demo1_1.add1)(2, 3));
+console.log("PI value " + demo1_1.pi);
---
+++ Typescript/Day07/demo2.ts
@@ -0,0 +1,9 @@
+import {Myclass,add1,pi} from './demo1';
+
+let obj=new Myclass(3,'Spruha','Raokhande');
+obj.display();
+
+
+console.log("Addition "+add1(2,3));
+
+console.log("PI value "+pi);
