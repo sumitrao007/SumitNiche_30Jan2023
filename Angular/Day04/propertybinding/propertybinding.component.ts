d4b212c6d1a06588df7a7ff5b86d8faa9461b53a
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-02-16 21:20:06 +0530

    Day04

---
+++ Angular/Day04/propertybinding/propertybinding.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-propertybinding',
+  templateUrl: './propertybinding.component.html',
+  styleUrls: ['./propertybinding.component.css']
+})
+export class PropertybindingComponent implements OnInit {
+
+  name:string='Sumit raokhande';
+  imgUrl:string='../../assets/bg4.jpg';
+  colSpanCount:number=2;
+  isHidden:boolean=false;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Angular/Day04/stylebinding/stylebinding.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stylebinding',
+  templateUrl: './stylebinding.component.html',
+  styleUrls: ['./stylebinding.component.css']
+})
+export class StylebindingComponent implements OnInit {
+
+  mycolor:string='red';
+  rating:number=21;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
